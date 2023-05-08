382f1b514171678a5aeb656e150c625957484549
abii225 <abhayv225@gmail.com>
2023-05-08 16:55:41 +0530

    added video on index

--- script/index.js
+++ script/index.js
@@ -48,11 +48,11 @@ $(document).ready(function() {
 //   testemoonials
 
         
-     
+     //----------->> Destinations  <<-------------------  
 
 let apiUrl=`https://trypot-nation.onrender.com/packages`;
 
-async function Fetchdata(){
+async function Fetchdata(apiUrl){
          try{
        let res=await fetch(apiUrl);
        let data=await res.json();
@@ -93,9 +93,10 @@ function displaydestination(obj){
         name.textContent=key;
         div.append(image,name);
         console.log(div);
+        // http://127.0.0.1:5500/destDetails.html?state=kerala
         div.addEventListener('click', () => {
             localStorage.setItem('package_id',obj[key][1]);
-            window.location.href = "user_package_view.html";
+            window.location.href = `http://127.0.0.1:5500/destDetails.html?state=${key}`;
         });
         destinationContainer.append(div);
     }
@@ -109,3 +110,74 @@ window.addEventListener("load",Fetchdata(apiUrl));
 
 
 
+
+
+
+
+
+
+// --------------->>>>>  Packages <<<<--------------
+
+
+//    ------>> activity <<<<----------------
+
+   let apiactivityUrl=`https://trypot-nation.onrender.com/activities`;
+
+   async function Fetchactivitydata(){
+            try{
+          let res=await fetch(apiactivityUrl);
+          let data=await res.json();
+         console.log(data);
+         activity(data);
+            }catch(err){
+               console.log(err);
+            }
+   }
+   
+   function activity(data){
+       // console.log(data);
+       let obj={};
+       for(let i=0;i<data.length;i++){
+           if(obj[data[i].category]===undefined){
+               let arr=[];
+               obj[data[i].category]=arr;
+               arr.push(data[i].image[0]);
+               arr.push(data[i].id);
+   
+           }
+       }
+       displayactivity(obj,data);
+   }
+   let activityContainer=document.getElementById("activity-container");
+   function displayactivity(obj,data){
+       console.log(obj);
+       // activityContainer.innerHTML="";
+       let objArr=Object.keys(obj);
+       console.log(objArr);
+       
+       for(let key in obj){
+           let div=document.createElement("div");
+           div.setAttribute("id",obj[key][1]);
+           let image=document.createElement("img");
+           image.src=obj[key][0];
+           let name=document.createElement("h3");
+           name.textContent=key;
+           div.append(image,name);
+           console.log(div);
+           div.addEventListener('click', () => {
+               localStorage.setItem('package_id',obj[key][1]);
+               window.location.href = `http://127.0.0.1:5500/${key}.html`;
+           });
+           activityContainer.append(div);
+       }
+      
+   
+   }
+   
+   window.addEventListener("load",Fetchactivitydata(apiactivityUrl));
+   
+   
+   
+   
+   
+   
